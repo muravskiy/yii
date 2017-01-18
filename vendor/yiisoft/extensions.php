132b561d7fb6eaf3e72eedc64a85f5453705cceb
muravskiy <i.muravskiy@gmail.com>
2017-01-18 09:28:00 +0200

    Старт)

---
+++ vendor/yiisoft/extensions.php
@@ -0,0 +1,15 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'zyx/zyx-phpmailer' => 
+  array (
+    'name' => 'zyx/zyx-phpmailer',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@zyx/phpmailer' => $vendorDir . '/zyx/zyx-phpmailer',
+    ),
+  ),
+);
