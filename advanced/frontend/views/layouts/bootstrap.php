e9ae453cc20538c97d5ddc8ca3a847999718533c
muravskiy <i.muravskiy@gmail.com>
2017-02-06 14:13:18 +0200

    Добавил дополнительные методы

--- advanced/frontend/views/layouts/bootstrap.php
+++ advanced/frontend/views/layouts/bootstrap.php
@@ -1,36 +1,24 @@
+<?php
+
+
+
+\frontend\assets\MainAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Realestate Bootstrap Theme </title>
-    <meta charset="UTF-8" />
+    <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <?= \yii\helpers\Html::csrfMetaTags() ?>
+    <?php $this->head() ?>
 
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css"  />
-    <link rel="stylesheet" href="assets/style.css" />
-    <script src="assets/js/jquery-1.9.1.min.js" ></script>
-    <script src="assets/bootstrap/js/bootstrap.js" ></script>
-    <script src="assets/script.js" ></script>
-
-
-
-    <!-- Owl stylesheet -->
-    <link rel="stylesheet" href="assets/owl-carousel/owl.carousel.css" >
-    <link rel="stylesheet" href="assets/owl-carousel/owl.theme.css" >
-    <script src="assets/owl-carousel/owl.carousel.js" ></script>
-    <!-- Owl stylesheet -->
-
-
-    <!-- slitslider -->
-    <link rel="stylesheet" type="text/css" href="assets/slitslider/css/style.css"  />
-    <link rel="stylesheet" type="text/css" href="assets/slitslider/css/custom.css"  />
-    <script type="text/javascript" src="assets/slitslider/js/modernizr.custom.79639.js" ></script>
-    <script type="text/javascript" src="assets/slitslider/js/jquery.ba-cond.min.js" ></script>
-    <script type="text/javascript" src="assets/slitslider/js/jquery.slitslider.js" ></script>
-    <!-- slitslider -->
-
-    <script src="assets/js/google_analytics_auto.js" ></script></head>
+</head>
 
 <body>
+<?php $this->beginBody() ?>
 
 
 <!-- Header Starts -->
@@ -461,6 +449,7 @@
 <!-- /.modal -->
 
 
-
+<?php $this->endBody() ?>
 </body>
 </html>
+<?php $this->endPage() ?>
